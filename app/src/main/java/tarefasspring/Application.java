a7ea7a33c79cebe3bcbd288c5064484acec63cb9
MatheusMorandi <theu.morandi@gmail.com>
2023-04-25 18:32:06 +0000

    Att

---
+++ app/src/main/java/tarefasspring/Application.java
@@ -0,0 +1,13 @@
+package tarefasspring;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
